5ab5265d56b4e82474b62aecef48a20a213a21a6
MORIHIKO-Mizuno <morimiz.business@gmail.com>
2023-08-17 23:15:30 +0900

    Added Shuffle functions.

--- shuffle.py
+++ shuffle.py
@@ -1,7 +1,7 @@
-
+from scipy.stats import kendalltau
 import random
 
-# リフルシャッフルする関数
+# パーフェクトシャッフルする関数
 def riffle_shuffle(deck):
     half_size = len(deck)//2
     first_half = deck[:half_size]
@@ -13,6 +13,29 @@ def riffle_shuffle(deck):
     # カードの枚数が奇数だった場合
     if len(first_half) > len(second_half):
         shuffled_deck.append(first_half[-1])
+    return shuffled_deck
+
+
+# ファローシャッフル(リフルシャッフル)する関数、パーフェクトではない
+def faro_shuffle(deck):
+    middle = len(deck) // 2
+    first_half = deck[:middle]
+    second_half = deck[middle:]
+    
+    shuffled_deck = []
+    while True:
+        try:
+            if random.random()<0.5:
+                shuffled_deck.append(first_half[0])
+                first_half.pop(0)
+            else:
+                shuffled_deck.append(second_half[0])
+                second_half.pop(0)
+        except IndexError:
+            shuffled_deck=shuffled_deck+first_half+second_half
+            break
+
+    
     return shuffled_deck
 
 
@@ -34,7 +57,13 @@ def hindu_shuffle(deck):
     return shuffled_deck
 
 
-# 元に戻すために必要なリフルシャッフルの回数を求める回数
+# ランダムシャッフルを行う関数(現実ではできない、順番を完全に混ぜる関数)
+def random_shuffle(deck):
+    random.shuffle(deck)
+    return deck
+
+
+# 元に戻すために必要なパーフェクトシャッフルの回数を求める回数
 def return_to_original_count(card_count):
     if card_count <= 0:
         raise ValueError("CardCount must be a positive integer.")
@@ -52,6 +81,12 @@ def return_to_original_count(card_count):
     else:
         card_count += 1
         return return_to_original_count(card_count)
+
+# どれほど混ざったかを確認する関数
+# ケンドールの順位相関係数 
+def calculate_kendall_tau(deck_before, deck_after):
+    correlation_coefficient, _ = kendalltau(deck_before, deck_after)
+    return correlation_coefficient
 
 
 # テスト用の例
